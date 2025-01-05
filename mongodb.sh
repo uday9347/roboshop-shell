696dbc32fc78727ebd35bf5c76c7a49795d98ec9
Uday Teja <udayakaramsetty@gmail.com>
2025-01-05 14:37:07 +0530

    new one

---
+++ mongodb.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+ID=$(id -u)
+TIMESTAMP=$(date +%F-%H-%M-%S)
+LOGFILE="$?-$TIMESTAMP.log"
+
+VALIDATE ()
+{
+    if [$1 -ne 0 ]
+    then 
+    echo "$2 is failed"
+    else 
+    echo "$2 is success"
+    fi 
+}
+
+if [ $ID -ne 0 ]
+then 
+    echo "switch to root user"
+    exit 1
+else 
+    echo "root user"
+fi 
+
+cp mongo.repo /etc/yum.repos.d/mongo.repo &>> $LOGFILE
+VALIDATE $? "copying mongo repo "
+
+
+dnf install mongodb-org -y  &>> $LOGFILE 
+VALIDATE $? "installong the mongodb"
+
+systemctl enable mongod  &>> $LOGFILE 
+VALIDATE $? "enabling the mongodb"
+
+systemctl start mongod  &>> $LOGFILE 
+VALIDATE $? "starting the mongodb"
+
+sed -i 's/127.0.0.1/0.0.0.0/g' /etc/mongod.conf &>> $LOGFILE
+VALIDATE $? "Remote server access"
+
+systemctl restart mongod &>> $LOGFILE 
+VALIDATE $? "starting the mongodb"
+
